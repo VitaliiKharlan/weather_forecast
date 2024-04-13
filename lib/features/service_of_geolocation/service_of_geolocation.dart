6669e48839f99ae174aaa0dea3135aaa8e766911
vitaliy.kharlan.1977@gmail.com <vitaliy.kharlan.1977.gmail.com>
2024-04-13 23:46:13 +0300

    add service of geolocation

--- lib/features/service_of_geolocation/service_of_geolocation.dart
+++ lib/features/service_of_geolocation/service_of_geolocation.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:weather_forecast/features/theme/app_colors.dart';
-import 'package:weather_forecast/features/theme/app_text_style.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../theme/app_colors.dart';
+import '../theme/app_text_style.dart';
 
 class ServiceOfGeolocation extends StatefulWidget {
   const ServiceOfGeolocation({super.key});
@@ -10,6 +13,64 @@ class ServiceOfGeolocation extends StatefulWidget {
 }
 
 class _ServiceOfGeolocationState extends State<ServiceOfGeolocation> {
+  Position? _currentLocation;
+  late bool servicePermission = false;
+  late LocationPermission permission;
+
+  String _currentAddress = '';
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  _getAddressFromCoordinates() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentLocation!.latitude, _currentLocation!.longitude);
+
+      Placemark place = placemarks[0];
+
+      setState(() {
+        _currentAddress = '${place.locality}, ${place.country}';
+      });
+    } catch (e) {
+      // print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,13 +133,33 @@ class _ServiceOfGeolocationState extends State<ServiceOfGeolocation> {
                 color: Colors.white.withOpacity(0.72),
               ),
             ),
+            // const SizedBox(height: 20),
+            // Text(
+            //   '$_currentLocation',
+            //   style: AppTextStyle.defaultTextDarkSemiBold.copyWith(
+            //     fontSize: 18,
+            //     fontWeight: FontWeight.w500,
+            //     color: Colors.white.withOpacity(0.72),
+            //   ),
+            // ),
             const SizedBox(height: 20),
-            Text('Coordinates',
+            Text(
+              'latitude = ${_currentLocation?.latitude}',
               style: AppTextStyle.defaultTextDarkSemiBold.copyWith(
                 fontSize: 18,
                 fontWeight: FontWeight.w500,
                 color: Colors.white.withOpacity(0.72),
-              ),),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'longitude = ${_currentLocation?.longitude}',
+              style: AppTextStyle.defaultTextDarkSemiBold.copyWith(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.white.withOpacity(0.72),
+              ),
+            ),
             const SizedBox(height: 48),
             Text(
               'Location Address',
@@ -89,19 +170,28 @@ class _ServiceOfGeolocationState extends State<ServiceOfGeolocation> {
               ),
             ),
             const SizedBox(height: 20),
-            Text('Address',
+            Text(
+              _currentAddress,
               style: AppTextStyle.defaultTextDarkSemiBold.copyWith(
                 fontSize: 18,
                 fontWeight: FontWeight.w500,
                 color: Colors.white.withOpacity(0.72),
-              ),),
+              ),
+            ),
             const SizedBox(height: 80),
             ElevatedButton(
-              onPressed: () {
-                print('test here');
+              onPressed: () async {
+                _currentLocation = await _getCurrentLocation();
+                // print('${_currentLocation!.latitude}');
+                // print('${_currentLocation!.longitude}');
+
+                await _getAddressFromCoordinates();
+                // print(_currentAddress);
+
+                // print('test here');
               },
               child: const Text('get Location'),
-            )
+            ),
           ],
         ),
       ),
--- lib/repositories/weather_details/air_pollution_repository.dart
+++ lib/repositories/weather_details/air_pollution_repository.dart
@@ -10,18 +10,9 @@ class AirPollutionRepository {
       double latCoord, double lonCoord) async {
     final response =
         await Dio().get('${_host}lat=$latCoord&lon=$lonCoord&$_appID$_apiKey');
-    // final response = await Dio().get(
-    //     'http://api.openweathermap.org/data/2.5/air_pollution?'
-    //         'lat=50.4500336&'
-    //         'lon=30.5241361&'
-    //         'appid='
-    //         '4dbe24134496b55a1b13855ddf7c5847');
 
-    // final data = response.data as Map<String, dynamic>;
     final data = response.data as Map<String, dynamic>;
 
-    // final airPollutionList = AirPollution.fromJson(data);
-    // final airPollutionList = AirPollution.fromJson(data);
     final airPollutionList = AirPollutionDetails.fromJson(data);
     // print(airPollutionList);
 
